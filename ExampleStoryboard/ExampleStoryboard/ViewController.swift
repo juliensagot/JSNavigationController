0398ffa6da26bd4011be21ad777fdb273f365d66
Julien Sagot <contact@juliensagot.fr>
2016-06-01 16:32:48 +0200

    Add ExampleStoryboard project

---
+++ ExampleStoryboard/ExampleStoryboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ExampleStoryboard
+//
+//  Created by Julien Sagot on 01/06/16.
+//  Copyright © 2016 Julien Sagot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
