0d82b583567a888fff85eed5c92a77e25464eb7b
Julien Sagot <contact@juliensagot.fr>
2016-06-05 18:33:22 +0200

    Add ExampleStoryboard project

---
+++ ExampleStoryboard/ExampleStoryboard/BasicNavigationBarViewController.swift
@@ -0,0 +1,48 @@
+import AppKit
+
+class BasicNavigationBarViewController: NSViewController {
+	@IBOutlet var backButton: NSButton?
+	@IBOutlet var titleLabel: NSTextField?
+	@IBOutlet var nextButton: NSButton?
+
+	// MARK: - View Lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.wantsLayer = true
+	}
+
+	override func viewDidAppear() {
+		super.viewDidAppear()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.superview?.addConstraints(viewConstraints())
+	}
+
+	override func viewWillDisappear() {
+		view.superview?.removeConstraints(viewConstraints())
+	}
+
+	// MARK: - Layout
+	private func viewConstraints() -> [NSLayoutConstraint] {
+		let left = NSLayoutConstraint(
+			item: view, attribute: .Left, relatedBy: .Equal,
+			toItem: view.superview, attribute: .Left,
+			multiplier: 1.0, constant: 0.0
+		)
+		let right = NSLayoutConstraint(
+			item: view, attribute: .Right, relatedBy: .Equal,
+			toItem: view.superview, attribute: .Right,
+			multiplier: 1.0, constant: 0.0
+		)
+		let top = NSLayoutConstraint(
+			item: view, attribute: .Top, relatedBy: .Equal,
+			toItem: view.superview, attribute: .Top,
+			multiplier: 1.0, constant: 0.0
+		)
+		let bottom = NSLayoutConstraint(
+			item: view, attribute: .Bottom, relatedBy: .Equal,
+			toItem: view.superview, attribute: .Bottom,
+			multiplier: 1.0, constant: 0.0
+		)
+		return [left, right, top, bottom]
+	}
+}
--- ExampleStoryboard/ExampleStoryboard/ViewController.swift
+++ ExampleStoryboard/ExampleStoryboard/ViewController.swift
@@ -1,27 +1,68 @@
-//
-//  ViewController.swift
-//  ExampleStoryboard
-//
-//  Created by Julien Sagot on 01/06/16.
-//  Copyright © 2016 Julien Sagot. All rights reserved.
-//
+import AppKit
+import JSNavigationController
 
-import Cocoa
+private extension Selector {
+	static let pushToNextViewController = #selector(ViewController.pushToNextViewController)
+	static let popViewController = #selector(ViewController.popViewController)
+}
 
-class ViewController: NSViewController {
+class ViewController: JSViewController {
 
+	// MARK: - View Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		// Do any additional setup after loading the view.
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.wantsLayer = true
+		view.layer?.backgroundColor = NSColor(deviceRed: 240/255, green: 240/255, blue: 240/255, alpha: 1.0).CGColor
 	}
 
-	override var representedObject: AnyObject? {
-		didSet {
-		// Update the view, if already loaded.
+	override func viewDidAppear() {
+		super.viewDidAppear()
+		view.superview?.addConstraints(viewConstraints())
+
+		// NavigationBar
+		(navigationBarVC as? BasicNavigationBarViewController)?.backButton?.target = self
+		(navigationBarVC as? BasicNavigationBarViewController)?.backButton?.action = .popViewController
+		(navigationBarVC as? BasicNavigationBarViewController)?.nextButton?.target = self
+		(navigationBarVC as? BasicNavigationBarViewController)?.nextButton?.action = .pushToNextViewController
+	}
+
+	override func viewDidDisappear() {
+		view.superview?.removeConstraints(viewConstraints())
+	}
+
+	// MARK: - Layout
+	private func viewConstraints() -> [NSLayoutConstraint] {
+		let left = NSLayoutConstraint(
+			item: view, attribute: .Left, relatedBy: .Equal,
+			toItem: view.superview, attribute: .Left,
+			multiplier: 1.0, constant: 0.0
+		)
+		let right = NSLayoutConstraint(
+			item: view, attribute: .Right, relatedBy: .Equal,
+			toItem: view.superview, attribute: .Right,
+			multiplier: 1.0, constant: 0.0
+		)
+		let top = NSLayoutConstraint(
+			item: view, attribute: .Top, relatedBy: .Equal,
+			toItem: view.superview, attribute: .Top,
+			multiplier: 1.0, constant: 0.0
+		)
+		let bottom = NSLayoutConstraint(
+			item: view, attribute: .Bottom, relatedBy: .Equal,
+			toItem: view.superview, attribute: .Bottom,
+			multiplier: 1.0, constant: 0.0
+		)
+		return [left, right, top, bottom]
+	}
+
+	func pushToNextViewController() {
+		if let destinationViewController = destinationViewController {
+			navigationController?.push(viewController: destinationViewController, animated: true)
 		}
 	}
 
-
+	func popViewController() {
+		navigationController?.popViewController(animated: true)
+	}
 }
-
